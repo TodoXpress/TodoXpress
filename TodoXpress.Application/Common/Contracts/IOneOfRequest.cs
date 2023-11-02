1400d25d461c7ae473bd31b58c600ab9290d8708
fabian <famabiker@gmail.com>
2023-11-02 17:56:01 +0100

    add mediatr and oneof abstraction

---
+++ TodoXpress.Application/Common/Contracts/IOneOfRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using OneOf;
+using TodoXpress.Domain;
+
+namespace TodoXpress.Application;
+
+/// <summary>
+/// Defines a request for a mediatR handler.
+/// </summary>
+/// <typeparam name="TResponse">The type of the successfull response from the handler.</typeparam>
+public interface IOneOfRequest<TResponse> 
+    : IRequest<OneOf<TResponse, IError>>
+{
+
+}
---
+++ TodoXpress.Application/Common/Contracts/IOneOfRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using OneOf;
+using TodoXpress.Domain;
+
+namespace TodoXpress.Application;
+
+/// <summary>
+/// Defines a handler of a mediatR request.
+/// </summary>
+/// <typeparam name="TRequest">The type of the request.</typeparam>
+/// <typeparam name="TResponse">The type of the successfull response.</typeparam>
+public interface IOneOfRequestHandler<TRequest, TResponse> 
+    : IRequestHandler<TRequest, OneOf<TResponse, IError>>
+    where TRequest : IRequest<OneOf<TResponse, IError>>
+{
+
+}
