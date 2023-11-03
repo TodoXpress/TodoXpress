3ac045cc916790351881ce51fdd4cf6ff715cea6
fabian <famabiker@gmail.com>
2023-11-03 13:30:00 +0100

    add error response

---
+++ TodoXpress.Api.Data/Responses/ErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using TodoXpress.Domain;
+
+namespace TodoXpress.Api.Data;
+
+public readonly record struct ErrorResponse
+{
+    public string ErrorType { get; init; }
+
+    public string Description { get; init; }
+
+    public bool IsSuccess => false;
+
+    public bool IsError => !IsSuccess;
+
+    public static ErrorResponse Create(IError error)
+    {
+        return new ErrorResponse()
+        {
+            ErrorType = error.Type.Name,
+            Description = error.Description
+        };
+    }
+}
