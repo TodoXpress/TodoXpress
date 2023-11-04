2d93376df68ec120883df462e6972aa89ce67704
fabian <famabiker@gmail.com>
2023-11-04 18:28:32 +0100

    add full handler

--- TodoXpress.Application/CalendarDomain/Calendars/Commands/CreateCalendar/CreateCalendarCommand.cs
+++ TodoXpress.Application/CalendarDomain/Calendars/Commands/CreateCalendar/CreateCalendarCommand.cs
@@ -1,11 +1,14 @@
 ﻿using System.Drawing;
 using TodoXpress.Application.Contracts.MediatR;
+using TodoXpress.Domain.Calendars;
 
 namespace TodoXpress.Application.CalendarDomain.Calendars.Commands.CreateCalendar;
 
 public record class CreateCalendarCommand : IOneOfRequest<Guid>
 {
     public required string Name { get; set; }
+
+    public required User User { get; set; }
 
     public Color Color { get; set; }
 }
--- TodoXpress.Application/CalendarDomain/Calendars/Commands/CreateCalendar/CreateCalendarCommandHandler.cs
+++ TodoXpress.Application/CalendarDomain/Calendars/Commands/CreateCalendar/CreateCalendarCommandHandler.cs
@@ -1,23 +1,49 @@
-﻿using MediatR;
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using OneOf;
 using TodoXpress.Application.Contracts.MediatR;
+using TodoXpress.Application.Contracts.Persistence;
 using TodoXpress.Domain;
+using TodoXpress.Domain.Calendars;
 
 namespace TodoXpress.Application.CalendarDomain.Calendars.Commands.CreateCalendar;
 
 public class CreateCalendarCommandHandler : IOneOfRequestHandler<CreateCalendarCommand, Guid>
 {
-    public Task<OneOf<Guid, IError>> Handle(CreateCalendarCommand request, CancellationToken cancellationToken)
+    IValidator<CreateCalendarCommand> validator;
+    ICalendarDataService calendarService;
+
+    public CreateCalendarCommandHandler(IValidator<CreateCalendarCommand> validator, ICalendarDataService calendarData)
+    {
+        this.validator = validator;
+        this.calendarService = calendarData;
+    }
+
+    public async Task<OneOf<Guid, IError>> Handle(CreateCalendarCommand request, CancellationToken cancellationToken)
     {
         // validate request
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
-        // check if calendar exists
+        if (!validationResult.IsValid)
+        {
+            return new ValidationError<ValidationResult>()
+            {
+                ValidationErrorDescription = validationResult.ToString("|")
+            };
+        }
+
+        var calendar = new Calendar()
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Color = request.Color,
+            Owner = request.User
+        };
 
         // save calendar
-
-        // return id of the calendar
-
-        return default;
+        var calendarId = await calendarService.CreateAsync(calendar);
+        
+        return calendarId;
     }
 
 }
