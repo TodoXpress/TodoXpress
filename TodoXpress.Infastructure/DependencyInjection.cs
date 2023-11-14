f630821d8a70ead5680d64347493d88c413b2014
Fabian Dasler <f.dasler@smf.de>
2023-11-14 14:34:32 +0100

    change place for extention methods

--- TodoXpress.Infastructure/DependencyInjection.cs
+++ TodoXpress.Infastructure/DependencyInjection.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using TodoXpress.Application.Contracts.Persistence.Services;
 using TodoXpress.Application.Contracts.Persistence;
 using TodoXpress.Infastructure.Persistence.Services.Calendars;
+using TodoXpress.Infastructure.Persistence.Contexts;
+using TodoXpress.Infastructure.Validation.Calendar;
+using FluentValidation;
 
 namespace TodoXpress.Infastructure;
 
@@ -17,6 +22,32 @@ public static class DependencyInjection
         services.AddScoped<ICalendarUnitOfWork, CalendarUnitOfWork>();
         services.AddScoped<ICalendarDataService, CalendarService>();
         services.AddScoped<ICalendarUserDataService, CalendarUserService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the DbContext for the Calendar Domain.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="config">The <see cref="IConfiguration"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddCalendarDbContext(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<CalendarDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("Default")));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the validators from Fluentvalidation.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddFluentValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<CreateCalendarValidator>(ServiceLifetime.Singleton);
 
         return services;
     }
